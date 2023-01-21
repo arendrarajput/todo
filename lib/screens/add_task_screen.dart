07a5f6874932c15f354ed6504e0dd92c7f855400
Sushant Kumar <sushant.kumar@logistiq.io>
2023-01-21 11:31:43 +0530

    Initial commit

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/tasks_screen.dart';
+import 'package:todoey_flutter/models/task.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    late String newTaskTitle;
+    return Container(
+      color: const Color(0xFF757575),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              onChanged: (newText) {
+                newTaskTitle = newText;
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final task = Task(name: newTaskTitle);
+                if (task != null) {
+                  // Provider.of<TaskData>(context, listen: false).tasks.add(task);
+                  Provider.of<TaskData>(context, listen: false).addTask(
+                      newTaskTitle); //to call notifylistner everytime tasks chages
+                }
+                Navigator.pop(context);
+              },
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
